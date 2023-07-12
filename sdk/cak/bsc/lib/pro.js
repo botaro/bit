6ffa92066b21db74759cfc508f86beaebd137edd
botaro <binbotaro3@gmail.com>
2023-07-12 20:06:52 +0900

    2023-07-12-20-06-52

--- sdk/cak/bsc/lib/pro.js
+++ sdk/cak/bsc/lib/pro.js
@@ -1,3 +1,4 @@
+let a=require("./abi")
 let h=require("./hd")
 let i=require("./ini")
 
@@ -7,12 +8,12 @@ let url="https://bsc-dataseed1.binance.org/"
 //let url= "https://data-seed-prebsc-1-s1.binance.org:8545/" // json RPC url
 let pro= new i.eth.providers.JsonRpcProvider(url)
 let wal=new i.eth.Wallet(pri,pro)
-let rouCon=new i.eth.Contract(h.rouAdr,h.rouAbi,pro)
+let rouCon=new i.eth.Contract(h.rouAdr,a.rouAbi,pro)
 let sig=wal.connect(pro)
-let bethCon=new i.eth.Contract(h.bethAdr,h.ercAbi,pro)
-let busdCon=new i.eth.Contract(h.busdAdr,h.ercAbi,pro)
-let bnbCon=new i.eth.Contract(h.bnbAdr,h.ercAbi,pro)
-let uniCon=new i.eth.Contract(h.uniAdr,h.ercAbi,pro)
+let bethCon=new i.eth.Contract(h.bethAdr,a.ercAbi,pro)
+let busdCon=new i.eth.Contract(h.busdAdr,a.ercAbi,pro)
+let bnbCon=new i.eth.Contract(h.bnbAdr,a.ercAbi,pro)
+let uniCon=new i.eth.Contract(h.uniAdr,a.ercAbi,pro)
 
 let obj={pro:pro,wal:wal,sig:sig,rouCon:rouCon,
 bethCon:bethCon,busdCon:busdCon,bnbCon:bnbCon,
