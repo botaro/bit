ff1a0ae9587bd716c40261474f73c7dd3f68092c
botaro <binbotaro3@gmail.com>
2023-07-13 21:15:45 +0900

    2023-07-13-21-15-45

--- bsc/lib/dec.js
+++
@@ -1,7 +0,0 @@
-let i=require("./ini")
-let h=require("./hd")
-let cid=5
-let dec=18
-let busd=new i.tok(cid,h.uni,dec)
-
-module.exports=busd
--- bsc/lib/ust.js
+++
@@ -1,7 +0,0 @@
-let i=require("./ini")
-let h=require("./hd")
-let cid=1
-let dec=18
-let ust=new i.tok(cid,h.ust,dec)
-
-module.exports=ust
